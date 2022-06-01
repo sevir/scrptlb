98337aa6ae9bf120aa0887630d7346ff8ee90f69
José F. Rives <jose@digio.es>
2022-06-02 00:03:23 +0200

    docs: Add more example code

--- scrptlb.code.lua
+++ scrptlb.code.lua
@@ -4,3 +4,11 @@ print(hello_message)
 local request = http.get("https://www.digio.es")
 print(request["status"])
 print(request["body"])
+
+http.add_header("Authorization", "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y")
+http.add_header("Content-Type", "application/json")
+
+http.set_body("Hello World")
+local request = http.post("https://reqbin.com/echo/post/json")
+print(request["status"])
+print(request["body"])
